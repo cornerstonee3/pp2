7087af130b1a98e5782406bf97e9f755bdf9cfbc
Amanbol <kanomru@gmail.com>
2025-09-13 21:50:13 +0500

    variables lesson

--- variables.py
+++ variables.py
@@ -1,3 +1,4 @@
+#Variables 1
 x = 5
 y = "John"
 print(x)
@@ -22,3 +23,89 @@ x = 'John'
 a = 4
 A = "Sally"
 #A will not overwrite a
+
+
+#Variable Names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+#Assign Multiple Values
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+#####
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+#####
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+#Output Variables
+x = "Python is awesome"
+print(x)
+###
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+####
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+####
+x = 5
+y = 10
+print(x + y)
+#####
+x = 5
+y = "John"
+print(x, y)
+
+
+#Global Variables
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+#####
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+######
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+#####
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
